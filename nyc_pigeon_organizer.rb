032086a581ca4c855f13f387ddd5cf4a54e77a61
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-28 04:35:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each do |key, parameters|
     parameters.each do |descriptions, names|
       names.each do |name|
-        
-          hash[name] ||= {}
-        
+        hash[name] ||= {}
         hash[name][key] ||= []
         hash[name][key].push(descriptions.to_s)
       end
