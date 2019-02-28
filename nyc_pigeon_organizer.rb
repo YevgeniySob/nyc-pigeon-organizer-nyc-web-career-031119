b90898c21af36eee09079915559fd72855acaf62
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-28 04:34:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+  data.each do |key, parameters|
+    parameters.each do |descriptions, names|
+      names.each do |name|
+        if !hash.has_key?(name)
+          hash[name] = {}
+        end
+        hash[name][key] ||= []
+        hash[name][key].push(descriptions.to_s)
+      end
+    end
+  end
+  hash
 end
